e567e7bdf6fa65e5dc7b58f1a6aed982536adfc3
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-08-08 07:59:06 +0100

    want to update it

--- src/routes/Counter.svelte
+++ src/routes/Counter.svelte
@@ -1,5 +1,6 @@
 <script>
 	let count =0;
+	let string = `You have clicked ${count} times`
 
 	function increment(){
 		count +=1;
@@ -8,3 +9,10 @@
 </script>
 
 <button on:click={increment}>Clicks {count}</button>
+<h3>{string}</h3>
+
+<style>
+	button{
+		padding:15px 20px;
+	}
+</style>
